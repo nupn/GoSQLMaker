e64f29d4f78084eba73930c343a81e5a6c1b4776
정영모 [nupn56] <nupn56@nexon.co.kr>
2018-02-23 19:34:23 +0900

    mod SQLConnector

--- GoSQLMaker/SQLConnector.go
+++ GoSQLMaker/SQLConnector.go
@@ -59,4 +59,62 @@ func main() {
 
 	defer rows.Close();
 } 
+
+
+func db() {
+    // sql.DB 객체 생성
+    db, err := sql.Open("mssql", "server=175.207.4.91; port=1433;user id=cocoro79;password=dbstkdgus123!;database=usercontent")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+
+    rows, err := db.Query("SELECT TOP 10 * FROM [UserContent].[dbo].[FxCAJJangRankHistory] where season = ?", 201801)
+
+    cols, err := rows.Columns()
+    if err != nil {
+        panic(err.Error())
+    }
+
+    colTypes, err := rows.ColumnTypes()
+    if err != nil {
+        panic(err.Error())
+    }
+
+    vals := make([]sql.RawBytes, len(cols))
+    scanArgs := make([]interface{}, len(vals))
+
+    for i := range vals {
+        scanArgs[i] = &vals[i]
+    }
+
+    for rows.Next() {
+        err := rows.Scan(scanArgs...)
+
+        if err != nil {
+            panic(err.Error())
+        }
+
+        fmt.Print("hash: ", GetHashNo(string(vals[4]), 6), " ")
+
+        var value string
+        for i, col := range vals {
+
+            fmt.Print(colTypes[i].DatabaseTypeName(), " ")
+
+            if col == nil {
+                value = "NULL"
+            } else {
+                value = string(col)
+            }
+
+            fmt.Printf("%s: %#v ", cols[i], value)
+            //fmt.Print(cols[i], ": ", value, "\t")
+        }
+        fmt.Println("\n-----------------------------------")
+    }
+    if err = rows.Err(); err != nil {
+        panic(err.Error())
+    }
+}
 */
